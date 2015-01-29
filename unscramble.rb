2f74e8f9c595fce3d22e9250716c7c3c06d53988
GaryCarneiro <GaryCarneiro@github.com>
2015-01-29 18:52:14 +0530

    Mac compatibilty only

--- unscramble.rb
+++ unscramble.rb
@@ -1,16 +1,24 @@
 #!/usr/bin/env ruby
 
+#Input word is first word
 iword = ARGV[0].split(//)
  
 words = []
-list = File.read("sowpods.txt").split("\n")
+#Line seperator for Mac "\r\n"
+list = File.read("sowpods.txt").split("\r\n")
+#p list.size 
  
 for i in (1..iword.length)
     (iword.permutation(i).map &:join).each do |word|
+      #p word
         if list.include? word.upcase
+            #p word
             words << word
         end
     end
 end
- 
-p words.sort.uniq
+
+puts "Possible words are zero" if words.size == 0 
+
+words.sort.uniq!
+words.each { | x| puts x }
