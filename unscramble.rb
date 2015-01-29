648a90138d7997ca9e23cb4e149d9b00d4318d57
GaryCarneiro <GaryCarneiro@github.com>
2015-01-29 18:23:52 +0530

    Fixed Identation

--- unscramble.rb
+++ unscramble.rb
@@ -6,11 +6,11 @@ words = []
 list = File.read("sowpods.txt").split("\n")
  
 for i in (1..iword.length)
-               (iword.permutation(i).map &:join).each do |word|
-                              if list.include? word.upcase
-                  words << word
-                end
-               end
+    (iword.permutation(i).map &:join).each do |word|
+        if list.include? word.upcase
+            words << word
+        end
+    end
 end
  
 p words.sort.uniq
